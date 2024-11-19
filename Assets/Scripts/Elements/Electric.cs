0f4451b5d219ade33d9a136b3a7455439318e039
Heng Nenghak <nenghak.heng@student.cadt.edu.kh>
2024-11-20 04:12:59 +0700

    close #4 add lightning spell particle system

--- Assets/Scripts/Elements/Electric.cs
+++ Assets/Scripts/Elements/Electric.cs
@@ -4,9 +4,24 @@ using UnityEngine;
 
 public class Electric : Projectile
 {
+    // void OnTriggerEnter(Collider other)
+    // {
+    //     Debug.Log($"Electric hit {other.gameObject.name}");
+    //     Destroy(gameObject);
+    // }
+
+    public ParticleSystem electricParticleSystem;
+    public float knockbackForce = 10f;
+
     void OnTriggerEnter(Collider other)
     {
         Debug.Log($"Electric hit {other.gameObject.name}");
         Destroy(gameObject);
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        Debug.Log($"Particle hit {other.name}");
+        // Add any additional logic you want to execute when a particle hits something
     }
 }
