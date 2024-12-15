d17ed51b6431367dd239e59ad0bc6a6d942ab8bd
Heng Nenghak <nenghak.heng@student.cadt.edu.kh>
2024-12-16 05:43:07 +0700

    close #18 implement fall damage

--- Assets/Scripts/Player/PlayerManager.cs
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -4,41 +4,92 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
-    public UIManager uiManager;
+    public UIManager uIManager;
     public Animator animator;
 
     [Header("Player Stats")]
     public float maxHealth = 200f;
     public float health;
 
-    private bool _alreadyDead;
+    [Header("Falling System")]
+    [SerializeField] 
+    private int fallDamageThreshold = 2; // Minimum fall distance to take damage
 
-    // Start is called before the first frame update
+    [SerializeField]
+    private float fallDamageMultiplier = 20f; // Damage multiplier based on fall distance
+
+    [SerializeField]
+    private int maxFallDistance = 8; // Maximum fall distance without taking fatal damage
+
+    private Vector3 lastGroundedPosition;
+    private bool isFalling = false;
+    private float fallStartHeight;
+
     void Start()
     {
         health = maxHealth;
-        uiManager.UpdateHealth(maxHealth, maxHealth);
+        uIManager.UpdateHealth(maxHealth, maxHealth);
         animator = GetComponent<Animator>();
+
+        Debug.Log($"Initial Fall Damage Threshold: {fallDamageThreshold:F3}");
+    }
+
+    void Update()
+    {
+        CheckFalling();
+    }
+
+    private void CheckFalling()
+    {
+        RaycastHit hit;
+        bool isGrounded = Physics.Raycast(transform.position, Vector3.down, out hit, 1.1f);
+
+        if (!isGrounded && !isFalling)
+        {
+            isFalling = true;
+            fallStartHeight = transform.position.y;
+        }
+        else if (isGrounded && isFalling)
+        {
+            float fallDistance = fallStartHeight - transform.position.y;
+            HandleFallDamage(fallDistance);
+            isFalling = false;
+        }
+    }
+
+    private void HandleFallDamage(float fallDistance)
+    {
+        if (fallDistance > fallDamageThreshold)
+        {
+            float damage = (fallDistance - fallDamageThreshold) * fallDamageMultiplier;
+
+            if (fallDistance < maxFallDistance)
+            {
+                TakeDamage(damage);
+            }
+            else
+            {
+                Debug.Log("Fatal Fall!");
+                health = 0;
+                Die();
+            }
+
+            Debug.Log($"Fall Distance: {fallDistance}, Damage: {damage}");
+        }
     }
 
     public void TakeDamage(float damage)
     {
         health -= damage;
-        uiManager.UpdateHealth(health, maxHealth);
-
-        if (health <= 0 && !_alreadyDead)
-        {
-            _alreadyDead = true;
-            Die();
-        }
+        uIManager.UpdateHealth(health, maxHealth);
+        if (health <= 0) Die();
     }
 
     private void Die()
     {
         Debug.Log("You Died");
-        // Update the enemy team score
         GameManager.Instance.UpdateScore(gameObject.CompareTag("Enemy"));
-        uiManager.GameHub.SetActive(false);
+        uIManager.GameHub.SetActive(false);
 
         if (animator != null)
         {
